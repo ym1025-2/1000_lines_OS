6a9f46462e7d8b7c56bbeb6d7d873a682c8bd1dd
ym1025-2 <234549032+ym1025-2@users.noreply.github.com>
2026-06-13 10:55:46 +0900

    added kernel

--- run.sh
+++ run.sh
@@ -4,5 +4,15 @@ set -xue
 # QEMUのファイルパス
 QEMU=qemu-system-riscv32
 
+# clangのパス (Ubuntuの場合は CC=clang)
+CC=clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+
+# カーネルをビルド
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
 # QEMUを起動
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
